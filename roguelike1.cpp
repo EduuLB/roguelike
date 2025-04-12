f95e44eb3dfa65cb17e4eba62515d66eb742b3d1
Eduardo Leopoldo <77700457+EduuLB@users.noreply.github.com>
2025-04-12 08:36:42 -0300

    ++inimigos
    Adicionado todos os inimigos e andam aleatoriamente

--- roguelike1.cpp
+++ roguelike1.cpp
@@ -8,38 +8,8 @@
 #include <chrono>
 using namespace std;
 
-int mapaEscuro[30][60] =  {
-    {1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4, 5, 4, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0},
-{1, 0, 0, 0, 0, 0, 8, 1, 0, 0, 4, 4, 4, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4, 5, 4, 0, 0, 0, 4, 4, 1, 0, 0, 0, 1, 0, 0, 0, 7, 1},
-{1, 7, 0, 0, 1, 1, 1, 1, 4, 4, 5, 5, 5, 4, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4, 4, 4, 4, 5, 4, 4, 4, 4, 5, 5, 0, 0, 1, 0, 0, 0, 1, 0, 0, 1},
-{1, 0, 0, 1, 1, 0, 0, 3, 5, 5, 5, 4, 5, 4, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 1, 1, 1, 4, 4, 4, 4, 4, 4, 4, 4, 4, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 4, 1, 0, 1, 0, 0, 1, 0, 0, 6, 1},
-{1, 1, 0, 0, 1, 0, 0, 1, 4, 4, 4, 4, 5, 4, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 4, 4, 4, 4, 5, 4, 4, 4, 4, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0},
-{1, 0, 0, 0, 0, 0, 6, 1, 0, 0, 0, 4, 5, 5, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 0, 0, 0, 4, 5, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-{1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 4, 4, 1, 7, 7, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-{1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 7, 0, 0, 0, 0, 1, 4, 4, 4, 4, 4, 4, 4, 4, 4, 0, 0, 0, 0},
-{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 5, 5, 5, 5, 5, 5, 5, 5, 5, 4, 0, 0, 0},
-{0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 1, 4, 4, 4, 4, 4, 4, 4, 5, 4, 0, 0, 0},
-{0, 0, 0, 0, 1, 6, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 4, 5, 4, 0, 0, 0},
-{0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 4, 4, 4, 4, 4, 4, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 4, 5, 4, 0, 0, 0},
-{0, 4, 4, 1, 0, 0, 1, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 5, 5, 5, 5, 5, 5, 4, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 5, 1, 1, 1, 0},
-{4, 5, 5, 5, 0, 0, 0, 0, 0, 0, 1, 0, 0, 7, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 4, 4, 4, 4, 4, 5, 5, 5, 4, 4, 4, 4, 4, 4, 4, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 7, 1},
-{4, 5, 4, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 7, 0, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 4, 4, 5, 5, 5, 5, 5, 5, 5, 5, 4, 4, 4, 4, 1, 1, 0, 1, 0, 0, 0, 1, 1, 0, 0, 1},
-{4, 5, 4, 0, 1, 9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 4, 4, 4, 4, 4, 4, 4, 5, 5, 5, 5, 5, 0, 0, 0, 0, 0, 0, 0, 0, 6, 1, 1, 0},
-{4, 5, 4, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4, 4, 4, 4, 4, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0},
-{4, 5, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-{4, 5, 5, 4, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0},
-{0, 4, 5, 4, 4, 4, 4, 4, 4, 4, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 4, 4, 4, 4, 4, 4, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0},
-{0, 4, 5, 5, 5, 5, 5, 5, 5, 5, 4, 4, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 0, 1, 0, 0, 0, 1, 1, 0, 1, 0, 0, 0, 5, 5, 5, 5, 5, 5, 4, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0},
-{0, 0, 4, 4, 5, 4, 4, 4, 4, 5, 5, 5, 5, 5, 5, 5, 5, 5, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 10, 0, 0, 0, 0, 1, 4, 4, 4, 4, 4, 5, 4, 0, 1, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-{1, 1, 1, 1, 0, 1, 1, 1, 4, 5, 4, 4, 4, 4, 4, 4, 4, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 4, 5, 4, 4, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-{1, 6, 0, 1, 0, 1, 0, 1, 4, 5, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 4, 5, 5, 5, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 11, 0, 0, 0, 1},
-{1, 0, 0, 1, 0, 1, 0, 1, 4, 5, 4, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 4, 5, 5, 5, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 11, 0, 0, 0, 1},
-{1, 0, 0, 0, 0, 0, 0, 1, 4, 5, 4, 4, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 4, 4, 4, 4, 4, 5, 4, 4, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-{1, 6, 1, 0, 0, 0, 1, 1, 4, 5, 5, 5, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 1, 0, 0, 1, 1, 1, 0, 5, 5, 5, 5, 5, 5, 5, 4, 0, 1, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-{1, 1, 1, 1, 1, 1, 1, 1, 0, 4, 4, 4, 1, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 1, 4, 4, 4, 4, 4, 4, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0},
-{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0},
-{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0}
-};
+int mapaEscuro[30][60] =  {0};
+
 int mapa[30][60] = {
 {1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4, 5, 4, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0},
 {1, 0, 0, 0, 0, 0, 8, 1, 0, 0, 4, 4, 4, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4, 5, 4, 0, 0, 0, 4, 4, 1, 0, 0, 0, 1, 0, 0, 0, 7, 1},
@@ -110,11 +80,11 @@ void centralizarTexto(const string& text) {
 int venceu(Jogador jogador){
     cout << "\n\n\n";
      cout << R"(
-					    ____   _    ____      _    ____  _____ _   _ ____  
-					   |  _ \ / \  |  _ \    / \  | __ )| ____| \ | / ___| 
-					   | |_) / _ \ | |_) |  / _ \ |  _ \|  _| |  \| \___ \ 
-					   |  __/ ___ \|  _ <  / ___ \| |_) | |___| |\  |___) |
-					   |_| /_/   \_\_| \_\/_/   \_\____/|_____|_| \_|____/ 
+					    __   _    __      _    __  __ _   _ ___
+					   |  _ \ / \  |  _ \    / \  | _ )| __| \ | / __|
+					   | |) / _ \ | |) |  / _ \ |  _ \|  | |  \| \__ \
+					   |  _/ __ \|  _ <  / _ \| |) | |_| |\  |__) |
+					   || //   \\| \\//   \\_/|__|| \|_/
     )" << endl;
     cout << "\n\n Voce derrotou o boss Univalison!! \n";
 	cout << "\n A sua Pontuacao Final foi de: ";
@@ -140,32 +110,29 @@ void abrirPorta(int& chave, int x, int y)
     }
 }
 
-void moverInimigo(Inimigo& inimigo) {
+void moverInimigo(Inimigo inimigos[2]) {
     int direcao = rand() % 4; // 0: cima, 1: baixo, 2: esquerda, 3: direita
-    int novaX = inimigo.x, novaY = inimigo.y;
 
+    for (int indexInimigo = 0; indexInimigo <= 6; indexInimigo++)
     switch (direcao) {
     case 0: // cima
-        if (mapa[inimigo.x - 1][inimigo.y] == 0)
-            novaX--;
+        if (mapa[inimigos[indexInimigo].x - 1][inimigos[indexInimigo].y] == 0)
+            inimigos[indexInimigo].x--;
         break;
     case 1: // baixo
-        if (mapa[inimigo.x + 1][inimigo.y] == 0)
-            novaX++;
+        if (mapa[inimigos[indexInimigo].x + 1][inimigos[indexInimigo].y] == 0)
+            inimigos[indexInimigo].x++;
         break;
     case 2: // esquerda
-        if (mapa[inimigo.x][inimigo.y - 1] == 0)
-            novaY--;
+        if (mapa[inimigos[indexInimigo].x][inimigos[indexInimigo].y - 1] == 0)
+            inimigos[indexInimigo].y--;
         break;
     case 3: // direita
-        if (mapa[inimigo.x][inimigo.y + 1] == 0)
-            novaY++;
+        if (mapa[inimigos[indexInimigo].x][inimigos[indexInimigo].y + 1] == 0)
+            inimigos[indexInimigo].y++;
         break;
     }
 
-    // Atualiza a posi  o do inimigo
-    inimigo.x = novaX;
-    inimigo.y = novaY;
 }
 
 void batalha(Jogador& jogador, Inimigo& inimigo) {
@@ -250,7 +217,7 @@ void batalha(Jogador& jogador, Inimigo& inimigo) {
 
     }
 }
-
+/*
 void batalhaBoss(Jogador& jogador, Inimigo& boss) {
     while (jogador.vida > 0) {
         system("cls");
@@ -264,9 +231,6 @@ void batalhaBoss(Jogador& jogador, Inimigo& boss) {
         char escolha;
         cin >> escolha;
 
-
-        // -------------------------------> For Reproduzindo Toda A matriz pra conseguir limpar a tela a cada ataque, mas mantendo o mapa aparecendo,
-       // ----------------------------------> depois tentar de outro jeito por que assim o mapa aparece e volta dando um flick
         boss.acao = rand() % 3;
         if (escolha == '1') { // Pedra
             if (boss.acao == 0) {
@@ -315,14 +279,15 @@ void batalhaBoss(Jogador& jogador, Inimigo& boss) {
 
         }
         system("pause");
-        
+
 
 
         if (boss.vida <= 0)
         {
+            jogador.pontuacao += (jogador.vida * 150);
             jogador.pontuacao += 1000;
             system("cls");
-            
+
             // jogador.pontuacao += jogador.vida * 50;
             venceu(jogador);
             break;
@@ -332,13 +297,23 @@ void batalhaBoss(Jogador& jogador, Inimigo& boss) {
 
     }
 }
-
+*/
 int jogo() {
     srand(static_cast<unsigned int>(time(0)));
     Jogador jogador;
-    Inimigo inimigo = { 5, 1, 1, 1, 2};
-    Inimigo inimigo2 = { 5, 4, 3, 1, 2};
-    Inimigo boss = {2, 0, 0, 1, 1};
+    Inimigo inimigos[7] = {
+        { 2, 1, 1, 1, 2}, // inimigo[0]
+        { 5, 2, 16, 1, 2}, // inimigo[1]
+        { 5, 8, 39, 1, 2}, // inimigo[2]
+        { 2, 15, 13, 1, 2}, // inimigo[3]
+	    { 2, 11, 10, 1, 2}, // inimigo[4]
+	    { 2, 20, 19, 1, 2}, // inimigo[5]
+	    { 2, 28, 28, 1, 2}, // inimigo[6]
+
+
+
+    };
+    Inimigo boss = {10, 0, 0, 2, 3};
 
     ///ALERTA: NAO MODIFICAR O TRECHO DE CODIGO, A SEGUIR.
     //INICIO: COMANDOS PARA QUE O CURSOR NAO FIQUE PISCANDO NA TELA
@@ -358,7 +333,7 @@ int jogo() {
 
     //Posicao inicial do inimigo no console
 
-    long long tempoAnterior = GetTickCount64(); // Tempo anterior do movimento do inimigo
+    long long tempoAnterior = GetTickCount(); // Tempo anterior do movimento do inimigo
     long long tempoAtual;
     //Variavel para tecla pressionada
     char tecla;
@@ -372,42 +347,93 @@ int jogo() {
         for (int i = 0; i < 30; i++) {
             for (int j = 0; j < 60; j++) {
                 if (i == jogador.x && j == jogador.y) {
-                    cout << char(190); //personagem
-                }
-                else if (i == inimigo.x && j == inimigo.y || i == inimigo2.x && j == inimigo2.y) {
-                    cout << char(38); // inimigo
-                }
-                else {
+                    cout << char(190); // personagem
+                } else if (
+                    i == inimigos[0].x &&
+                    j == inimigos[0].y ||
+                    i == inimigos[1].x &&
+                    j == inimigos[1].y ||
+                    i == inimigos[2].x &&
+                    j == inimigos[2].y ||
+                    i == inimigos[3].x &&
+                    j == inimigos[3].y ||
+                    i == inimigos[4].x &&
+                    j == inimigos[4].y ||
+                    i == inimigos[5].x &&
+                    j == inimigos[5].y ||
+                    i == inimigos[6].x &&
+                    j == inimigos[6].y
+              
+                ) {
+                    bool inimigoImpresso = false; // Variável de controle
+
+                    for (int controle = 0; controle < 2; controle++) {
+                        for (int di = -3; di <= 3 && !inimigoImpresso; di++) {
+                            for (int dj = -3; dj <= 3; dj++) {
+                                int posX = inimigos[controle].x + di;
+                                int posY = inimigos[controle].y + dj;
+
+                                // Verifica se as coordenadas estão dentro do mapa
+                                if (posX >= 0 && posY >= 0 && mapa[posX] != 0) {
+                                    // Calcula a distância e verifica raio
+                                    int distanciaX = (jogador.x - posX >= 0) ? jogador.x - posX : -(jogador.x - posX);
+                                    int distanciaY = (jogador.y - posY >= 0) ? jogador.y - posY : -(jogador.y - posY);
+
+                                    if (distanciaX + distanciaY <= 3) { // Condição de raio
+                                        cout << char(38); // Imprime o inimigo
+                                        inimigoImpresso = true; // Atualiza a variável de controle
+                                        break; // Interrompe o loop interno
+                                    }
+                                }
+                            }
+
+                            if (inimigoImpresso) break; // Interrompe o loop externo
+                        }
+
+                        if (!inimigoImpresso) {
+                            cout << " "; // Imprime espaço vazio caso o inimigo não esteja no raio
+                        }
+                    }
+                } else {
+                    // Garantir que sempre seja impresso algo no mapa
                     switch (mapaEscuro[i][j]) {
-                    case 0: cout << " "; break; //caminho
-                    case 1: cout << char(219); break; //parede
-                    case 2: cout << " "; break; // 
-                    case 3: cout << char(178); break; //Porta Fechada
-                    case 4: cout << " "; break; // Espa�o Bloqueado
+                    case 0: cout << " "; break; // caminho
+                    case 1: cout << char(219); break; // parede
+                    case 2: cout << " "; break; //
+                    case 3: cout << char(178); break; // Porta Fechada
+                    case 4: cout << " "; break; // Espaço Bloqueado
                     case 5: cout << char(176); break; // Caminho entre as salas
-                    case 6: cout << char(117); break; // pocao
+                    case 6: cout << char(117); break; // poção
                     case 7: cout << char(88); break; // Armadilha
                     case 8: cout << char(170); break; // Chave
                     case 9: cout << char(184); break; // aumentar dano
                     case 10: cout << char(241); break; // mapa
                     case 11: cout << char(157); break; // BOSS
-                    } //fim switch
+                    }
                 }
+
             }
             cout << "\n";
         } //fim for mapa
-        cout << "\n Vidas: " << jogador.vida << "  ||  Pontuacao: " << jogador.pontuacao << " || Usos da Pocao: " << jogador.pocao << " || Chave: " << jogador.chave;
+        cout << "\n Vidas: ";
+    	cout << jogador.vida;
+    	cout << "  ||  Pontuacao: ";
+    	cout << jogador.pontuacao;
+    	cout <<  "  || Usos da Pocao: ";
+    	cout << jogador.pocao;
+    	cout << "  || Chave: " ;
+    	cout << jogador.chave;
 
-        if (mapa[jogador.x][jogador.y] == 6) { // jogador pegar pocao
+        if (mapa[jogador.x][jogador.y] == 6) { // pocao
             jogador.pocao += 3;
             jogador.pontuacao += 50;
             mapa[jogador.x][jogador.y] = 0;
         }
-        if (mapa[jogador.x][jogador.y] == 7) { // jogador chegar em uma armadilha
+        if (mapa[jogador.x][jogador.y] == 7) { // armadilha
             jogador.vida -= 5;
             mapa[jogador.x][jogador.y] = 0;
         }
-        if (mapa[jogador.x][jogador.y] == 8) { // jogador pegar uma chave
+        if (mapa[jogador.x][jogador.y] == 8) { // chave
             jogador.chave = true;
             mapa[jogador.x][jogador.y] = 0;
             jogador.pontuacao += 50;
@@ -416,40 +442,39 @@ int jogo() {
         { // jogador chegar perto de uma porta
             abrirPorta(jogador.chave, jogador.x, jogador.y);
         }
-        if (mapa[jogador.x][jogador.y] == 9)
+        if (mapa[jogador.x][jogador.y] == 9) // item aumentar dano
         {
 	        jogador.dano += 3;
             mapa[jogador.x][jogador.y] = 0;
             jogador.pontuacao += 50;
         }
-        if (mapa[jogador.x][jogador.y] == 10)
+        if (mapa[jogador.x][jogador.y] == 10) // item mapa
         {
-	        
             for (int linha = 0; linha < 30; linha ++) {
 	            for (int coluna = 0; coluna < 60; coluna++) {
 		            mapaEscuro[linha][coluna] = mapa[linha][coluna];
 	            }
             }
         	mapa[jogador.x][jogador.y] = 0;
-            
-        }
-        if (mapa[jogador.x + 1][jogador.y] == 11 || mapa[jogador.x - 1][jogador.y] == 11 || mapa[jogador.x][jogador.y + 1] == 11 || mapa[jogador.x][jogador.y - 1] == 11)
-        {
-	        batalhaBoss(jogador, boss);
-        }
-        
 
-        tempoAtual = GetTickCount64();
+        }
+        if (mapa[jogador.x + 1][jogador.y] == 11 || mapa[jogador.x - 1][jogador.y] == 11 || mapa[jogador.x][jogador.y + 1] == 11 || mapa[jogador.x][jogador.y - 1] == 11) 
+        { // boss
+	      //  batalhaBoss(jogador, boss);
+        }
+
+
+        tempoAtual = GetTickCount();
 
         // Se passou meio segundo ou millissegundos), move o inimigo
         if (tempoAtual - tempoAnterior >= 500) {
-            moverInimigo(inimigo);
+            moverInimigo(inimigos);
             tempoAnterior = tempoAtual; // Atualiza o tempo anterior para o tempo atual
         }
 
-        if (jogador.x == inimigo.x && jogador.y == inimigo.y) { // jogador encontra um inimigo
-            batalha(jogador, inimigo);
-        }
+      //  if (jogador.x == inimigo.x && jogador.y == inimigo.y) { // jogador encontra um inimigo
+        //    batalha(jogador, inimigo);
+        //}
 
         ///executa os movimentos
         if (_kbhit()) { // SE _kbhit == true significa que o usuario apertou ALGUM BOTAO, depois o getch pega essa tecla para jogar pro switch abaixo
@@ -496,6 +521,11 @@ int jogo() {
             cin >> escolha;
             if (escolha == 's' || escolha == 'S') {
                 system("cls");
+            	for (int linha = 0; linha < 30; linha ++) {
+	            for (int coluna = 0; coluna < 60; coluna++) {
+		            mapaEscuro[linha][coluna] = 0;
+	            }
+            }
                 return jogo();
             }
             else {
